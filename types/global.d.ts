a506b50980ede32cb988229299ceec6d97d5ea38
Pieterjan C <pieterjan.carly@gmail.com>
2019-09-19 16:14:59 +0200

    start upgrade

---
+++ types/global.d.ts
@@ -0,0 +1,6 @@
+// Types for compiled templates
+declare module 'ember-attribute-validations/templates/*' { 
+  import { TemplateFactory } from 'htmlbars-inline-precompile';
+  const tmpl: TemplateFactory;
+  export default tmpl;
+}
