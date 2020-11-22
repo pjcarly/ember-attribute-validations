181378858202632411d79b6ceb91421be9ed368c
Pieterjan C <pieterjan.carly@gmail.com>
2020-11-22 23:01:32 +0100

    Added Validator Service from Mixin

--- addon/mixins/validator.js
+++ addon/mixins/validator.js
@@ -1,55 +1,7 @@
 import Mixin from "@ember/object/mixin";
-import ValidationError from "../error";
-import { canInvoke } from "../utils";
-import { getOwner } from "@ember/application";
-import { isEmpty } from "@ember/utils";
-import { isArray } from "@ember/array";
-import { get } from "@ember/object";
-import { assert } from "@ember/debug";
-import { assign } from "@ember/polyfills";
-import { run } from "@ember/runloop";
-import { camelize } from "@ember/string";
 import { reject } from "rsvp";
 import { inject as service } from "@ember/service";
 
-function createValidationError(model, intl) {
-  const message = intl.t("ember-attribute-validations.error");
-  return new ValidationError(message, model.errors);
-}
-
-function lookupValidtorFactory(container, key) {
-  return (
-    container.factoryFor(`validator:${key}`).class ||
-    container.factoryFor(`ember-attribute-validations@validator:${key}`).class
-  );
-}
-
-function lookupValidator(container, validator) {
-  const typeKey = validator.type;
-  const validatorClass = lookupValidtorFactory(container, typeKey);
-
-  assert(
-    "Could not find Validator `" + typeKey + "`.",
-    typeof validatorClass === "function"
-  );
-
-  let value = validator.value;
-
-  if (typeof value !== "object") {
-    value = {};
-
-    value[validator.type] = validator.value;
-  }
-
-  assign(value, {
-    attribute: validator.attribute,
-    container: container,
-  });
-
-  validatorClass.typeKey = camelize(typeKey);
-
-  return validatorClass.create(value);
-}
 
 /**
  * Validator Mixin to be used on a DS.Model.
@@ -60,175 +12,29 @@ function lookupValidator(container, validator) {
  */
 export default Mixin.create({
   intl: service(),
+  validator: service(),
 
-  /**
-   * Resolves the List of Validators for a given attribute.
-   *
-   * @method validatorsFor
-   * @param  {Attribute}  attribute
-   * @return {Validator}
-   */
+
   validatorsFor(attribute) {
-    const meta = attribute.options;
-    let validations = get(meta, "validation");
-
-    if (isEmpty(validations)) {
-      return [];
-    }
-
-    if (!isArray(validations)) {
-      validations = [validations];
-    }
-
-    const validators = [];
-
-    validations.forEach((validation) => {
-      const keys = Object.keys(validation);
-
-      keys.forEach((name) => {
-        validators.push({
-          type: name,
-          value: validation[name],
-          attribute: attribute,
-        });
-      });
-    });
-
-    return validators.map((validator) => {
-      return lookupValidator(getOwner(this), validator);
-    });
+    this.validator.validatorsFor(attribute);
   },
 
-  /**
-   * Validate a single Attribute.
-   *
-   * If the Attribute has defined validation, it would try to resolve
-   * the the required Validators and run validation.
-   *
-   * For each failed validation, error message is added to the Errors
-   * object for it's attribute name.
-   *
-   * @method _validateAttribute
-   * @param  {Attribute} attribute
-   * @private
-   */
   _validateAttribute(attribute) {
-    const validators = this.validatorsFor(attribute);
-    const name = attribute.name;
-
-    // Assign the Model name to the Attribute
-    attribute.parentTypeKey =
-      this.constructor.modelName || this.constructor.typeKey;
-
-    validators.forEach((validator) => {
-      const result = validator.validate(name, this.get(name), attribute, this);
-
-      if (typeof result === "string") {
-        this._addValidationError(name, result);
-      }
-    });
+    this.validator._validateAttribute(this, attribute);
   },
 
-  /**
-   * Validate a single Relationship.
-   *
-   * If the Relationship has defined validation, it would try to resolve
-   * the the required Validators and run validation.
-   *
-   * For each failed validation, error message is added to the Errors
-   * object for it's relationship name.
-   *
-   * @method _validateAttribute
-   * @param  {Relationship} relationship
-   * @private
-   */
+
   _validateRelationship(relationship) {
-    const validators = this.validatorsFor(relationship);
-    const name = relationship.key;
-
-    relationship.parentTypeKey =
-      this.constructor.modelName || this.constructor.typeKey;
-
-    validators.forEach((validator) => {
-      const result = validator.validate(
-        name,
-        this.get(name),
-        relationship,
-        this
-      );
-
-      if (typeof result === "string") {
-        this._addValidationError(name, result);
-      }
-    });
+    this.validator._validateRelationship(this, relationship);
   },
 
-  /**
-   * Validates the Model.
-   *
-   * If the Model is valid, this method would return `true`.
-   *
-   * If the validation fails, Model Errors would be populated
-   * by validation errors and it would transition into an invalid
-   * state.
-   *
-   * @method validate
-   * @return {Boolean}
-   * @private
-   */
+
   validate() {
-    // Do not validate the records which are deleted
-    if (this.isDeleted) {
-      return true;
-    }
-
-    const errors = this.errors;
-    errors._clear();
-
-    // Clear the errors from the model and set the model
-    // into an `uncommitted` state if the model is invalid
-    if (!this.isValid) {
-      errors.trigger("becameValid");
-    }
-
-    this.eachAttribute((key, attribute) => {
-      run(this, "_validateAttribute", attribute);
-    });
-
-    this.eachRelationship((key, relationship) => {
-      run(this, "_validateRelationship", relationship);
-    });
-
-    const isValid = get(errors, "isEmpty");
-
-    // Move the model into an 'invalid' state if the errors
-    // are not empty
-    if (!isValid) {
-      errors.trigger("becameInvalid");
-    }
-
-    return isValid;
+    this.validator.validateModel(this);
   },
 
-  /**
-   * Adds a validation message to the model
-   *
-   * @method _addValidationError
-   * @param  {Attribute}  attribute
-   * @param  {String}  message
-   * @private
-   */
   _addValidationError(attribute, message) {
-    // If the model is in a Saved state, flag it as dirty.
-    if (!this.hasDirtyAttributes) {
-      this.send("becomeDirty");
-    }
-
-    if (canInvoke(this.errors, "_add")) {
-      this.errors._add(attribute, message);
-    } else {
-      this.errors.add(attribute, message);
-    }
+    this.validator._addValidationError(this, attribute, message);
   },
 
   /**
@@ -244,6 +50,6 @@ export default Mixin.create({
       return this._super();
     }
 
-    return reject(createValidationError(this, this.intl));
+    return reject(this.validator.createValidationError(this));
   },
 });
---
+++ addon/service/validator.ts
@@ -0,0 +1,212 @@
+import Model from "@ember-data/model";
+import { getOwner } from "@ember/application";
+import { isArray } from "@ember/array";
+import { assert } from "@ember/debug";
+import { computed } from "@ember/object";
+import { assign } from "@ember/polyfills";
+import { run } from "@ember/runloop";
+import Service from "@ember/service";
+import { inject as service } from "@ember/service";
+import { camelize } from "@ember/string";
+import { isEmpty } from "@ember/utils";
+import DS from "ember-data";
+import { AttributeInterface } from "../base-validator";
+import ValidationError from "../error";
+
+export default class ValidatorService extends Service {
+  @service intl!: any;
+
+  @computed()
+  get container(): any {
+    return getOwner(this);
+  }
+
+  lookupValidtorFactory(key: string) {
+    return (
+      this.container.factoryFor(`validator:${key}`).class ||
+      this.container.factoryFor(`ember-attribute-validations@validator:${key}`)
+        .class
+    );
+  }
+
+  lookupValidator(validator: any) {
+    const typeKey = validator.type;
+    const validatorClass = this.lookupValidtorFactory(typeKey);
+
+    assert(
+      "Could not find Validator `" + typeKey + "`.",
+      typeof validatorClass === "function"
+    );
+
+    let value = validator.value;
+
+    if (typeof value !== "object") {
+      value = {};
+
+      value[validator.type] = validator.value;
+    }
+
+    assign(value, {
+      attribute: validator.attribute,
+      container: this.container,
+    });
+
+    validatorClass.typeKey = camelize(typeKey);
+
+    return validatorClass.create(value);
+  }
+
+  /**
+   * Resolves the List of Validators for a given attribute.
+   */
+  validatorsFor(attribute: AttributeInterface) {
+    const meta = attribute.options;
+    let validations = meta?.validation;
+
+    if (isEmpty(validations)) {
+      return [];
+    }
+
+    if (!isArray(validations)) {
+      validations = [validations];
+    }
+
+    const validators: any[] = [];
+
+    validations.forEach((validation: any) => {
+      const keys = Object.keys(validation);
+
+      keys.forEach((name) => {
+        validators.push({
+          type: name,
+          value: validation[name],
+          attribute: attribute,
+        });
+      });
+    });
+
+    return validators.map((validator) => {
+      return this.lookupValidator(validator);
+    });
+  }
+
+  /**
+   * Validate a single Attribute.
+   *
+   * If the Attribute has defined validation, it would try to resolve
+   * the the required Validators and run validation.
+   *
+   * For each failed validation, error message is added to the Errors
+   * object for it's attribute name.
+   *
+   */
+  private _validateAttribute(model: Model, attribute: AttributeInterface) {
+    const validators = this.validatorsFor(attribute);
+    const name = attribute.name;
+
+    // Assign the Model name to the Attribute
+    attribute.parentTypeKey =
+      // @ts-ignore
+      model.constructor.modelName || model.constructor.typeKey;
+
+    validators.forEach((validator) => {
+      // @ts-ignore
+      const result = validator.validate(name, model.get(name), attribute, this);
+
+      if (typeof result === "string") {
+        this._addValidationError(model, name, result);
+      }
+    });
+  }
+
+  /**
+   * Validate a single Relationship.
+   *
+   * If the Relationship has defined validation, it would try to resolve
+   * the the required Validators and run validation.
+   *
+   * For each failed validation, error message is added to the Errors
+   * object for it's relationship name.
+   */
+  private _validateRelationship(
+    model: Model,
+    relationship: Parameters<Parameters<Model["eachRelationship"]>[0]>[1]
+  ) {
+    // @ts-ignore
+    const validators = this.validatorsFor(relationship);
+    const name = relationship.key;
+
+    // @ts-ignore
+    relationship.parentTypeKey =
+      // @ts-ignore
+      model.constructor.modelName || model.constructor.typeKey;
+
+    validators.forEach((validator) => {
+      const result = validator.validate(
+        name,
+        model.get(name),
+        relationship,
+        model
+      );
+
+      if (typeof result === "string") {
+        this._addValidationError(model, name, result);
+      }
+    });
+  }
+
+  /**
+   * Adds a validation message to the model
+   */
+  private _addValidationError(model: Model, attribute: any, message: string) {
+    // If the model is in a Saved state, flag it as dirty.
+    if (!model.hasDirtyAttributes) {
+      model
+        // @ts-ignore
+        .send("becomeDirty");
+    }
+
+    const errors = <DS.Errors>(<unknown>model.errors);
+    errors.add(attribute, message);
+  }
+
+  validateModel(model: Model): boolean {
+    // Do not validate the records which are deleted
+    if (<boolean>(<unknown>model.isDeleted)) {
+      return true;
+    }
+
+    const errors = <DS.Errors>(<unknown>model.errors);
+    errors.clear();
+
+    // Clear the errors from the model and set the model
+    // into an `uncommitted` state if the model is invalid
+    if (!model.isValid) {
+      errors.trigger("becameValid");
+    }
+
+    // @ts-ignore
+    model.eachAttribute((key: string, attribute: AttributeInterface) => {
+      run(this, this._validateAttribute, attribute);
+    });
+
+    model.eachRelationship((_, relationship) => {
+      run(this, this._validateRelationship, relationship);
+    });
+
+    const isValid = <boolean>(<unknown>errors.isEmpty);
+
+    // Move the model into an 'invalid' state if the errors
+    // are not empty
+    if (!isValid) {
+      errors.trigger("becameInvalid");
+    }
+
+    return isValid;
+  }
+
+  createValidationError(model: Model) {
+    const message = this.intl.t("ember-attribute-validations.error");
+    return new ValidationError(message, <DS.Errors>(<unknown>model.errors));
+  }
+}
