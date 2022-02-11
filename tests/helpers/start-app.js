cf41d34f79c1b2a4e728d5bd8ff200b01d88b278
Pieterjan C <pieterjan.carly@gmail.com>
2022-02-11 15:20:12 +0100

    deprecation

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -1,13 +1,12 @@
 import Application from '../../app';
 import config from '../../config/environment';
-import { assign } from '@ember/polyfills';
 import { run } from '@ember/runloop';
 
 export default function startApp(attrs) {
   var application;
 
-  var attributes = assign({}, config.APP);
-  attributes = assign(attributes, attrs); // use defaults, but you can override;
+  var attributes = Object.assign({}, config.APP);
+  attributes = Object.assign(attributes, attrs); // use defaults, but you can override;
 
   run(function() {
     application = Application.create(attributes);
